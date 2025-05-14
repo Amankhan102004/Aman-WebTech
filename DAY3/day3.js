20ea564c0899809d90c4d257215fa8e648cc4492
Aman102004 <amank5643142@gmail.com>
2025-05-14 15:25:16 +0530

    add file

---
+++ DAY3/day3.js
@@ -0,0 +1,25 @@
+// function testFunction(){
+//     console.log("Hello MCA II - A");
+// } 
+
+// function with arguments
+function testFunction(x,y){
+    return (x+y);
+}
+console.log(testfunction(10,20));
+
+// function with default value 
+
+// function testFunction(x=10,y){
+//     let z=x+y
+// }
+//     testFunction(5)
+//     return z
+
+
+   ///function as a expression-
+   let t=function testFunction(x,y){
+    return(x+y);
+   }
+   console.log(t(10,20));
+   
