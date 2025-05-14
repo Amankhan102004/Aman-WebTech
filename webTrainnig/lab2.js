52a265898719bb17ff12d9e6b10d7f19aee30a81
Aman102004 <amank5643142@gmail.com>
2025-05-14 15:41:50 +0530

    add

---
+++ webTrainnig/lab2.js
@@ -0,0 +1,11 @@
+//console.log("5"+2+3)
+//console.log(2+3+"4")
+// console.log('5'===5)
+// console.log("hello world")
+let greeter="Hey Arman";
+var times=4;
+if(times>3){
+    let greeter="say hello instead";
+    console.log(greeter);
+}
+console.log(greeter);
